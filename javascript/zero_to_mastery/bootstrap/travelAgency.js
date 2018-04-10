4d1ed504f254585c4a236a606a6c2a6d886937b6
Taylor Lassiter <lassit09@gmail.com>
2018-04-09 20:00:40 -0400

    still trying to work out push problems

--- javascript/zero_to_mastery/bootstrap/travelAgency.js
+++
@@ -1,9 +0,0 @@
-var links = document.querySelectorAll(".nav-item");
-
-function clickedLink() {
-    for(var i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", function(){
-            links[i].classList.add("selected");
-        });
-    }
-}

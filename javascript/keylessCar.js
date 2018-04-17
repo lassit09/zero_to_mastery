7e186fe1e7e2ac116c46b2df71cdef59c35bb291
Taylor Lassiter <lassit09@gmail.com>
2018-04-16 21:35:12 -0400

    create functions to refactor and improve the keyless car

--- javascript/keylessCar.js
+++ javascript/keylessCar.js
@@ -1,12 +1,24 @@
 //Make a keyless car!
 //This car will only let you dirve if you are over 18.
 
-var age = Number(prompt("What is your age?"));
+function checkDriverAge() {
+	var age = Number(prompt("What is your age?"));
 
-if (age < 18) {
-	alert("Sorry, you are too young to be driving this car. Powering off.");
-} else if (age === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
-} else {
-	alert("Powering on. Enjoy the ride!");
+	if (age < 18) {
+		alert("Sorry, you are too young to be driving this car. Powering off.");
+	} else if (age === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	} else {
+		alert("Powering on. Enjoy the ride!");
+	}
+}
+
+function checkDriverAge2(age) {
+	if (age < 18) {
+		return "Sorry, you are too young to be driving this car. Powering off.";
+	} else if (age === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	} else {
+		return "Powering on. Enjoy the ride!";
+	}
 }

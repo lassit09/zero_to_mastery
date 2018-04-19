5622011b1a262a9872af517f41f06f89ac9e419c
Taylor Lassiter <lassit09@gmail.com>
2018-04-18 21:44:39 -0400

    create facebook-like app with basic login and newsfeed

--- javascript/facebook.js
+++ javascript/facebook.js
@@ -5,17 +5,30 @@ var database = [
     }
 ];
 
-var newsfeed = [
+var newsFeed = [
     {
         username: 'klassi6',
-        timeline: 'tl1'
+        timeline: 'Great workout today!'
     },
     {
         username: 'jbowers12',
-        timeline: 'tl2'
+        timeline: 'Another long day at Prometric...'
     },
     {
         username: 'gwilly13',
-        timeline: 'tl3'
+        timeline: 'Clients drove me insane today!'
     }
 ]
+
+var userNamePrompt = prompt('What\'s your username?');
+var passwordPrompt = prompt('What\'s your password?');
+
+function signIn(username, password) {
+    if (username === database[0].username && password === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert('Sorry, wrong username or password!');
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);

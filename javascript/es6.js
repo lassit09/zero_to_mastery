2adc5a068432a81ee59c9e14d503c47e542b3d3b
Taylor Lassiter <lassit09@gmail.com>
2018-05-11 07:36:24 -0400

    more notes on ES6

--- javascript/es6.js
+++ javascript/es6.js
@@ -40,3 +40,27 @@ const obj = {
 }
 
 //TEMPLATE STRINGS
+const name = "Sally";
+const age = 34;
+const pet = "Horse";
+//old way
+const greeting = "Hello " + name + " you seem to be doing " + greeting + ".";
+//new way uses back ticks 
+const greetingBest = `Hello ${name} you seem to be ${age} years old.  What a lovely ${pet} you have!`;
+
+//DEFAULT ARGUMENTS
+function greet(name='', age=10, pet='cat') {
+    return `Hello, ${name}, you seem to be ${age}. What a lovely ${pet} you have!`;
+}
+
+//SYMBOLS (not used often)
+let sym1 = Symbol();
+let sym2 = Symbol('foo'); //these two will not be equal, they are unique
+let sym3 = Symbol('foo'); //these two will not be equal, they are unique
+
+//ARROW FUNCTIONS
+function add(a, b) {
+    return a + b;
+}
+
+const add2 = (a, b) => a + b;
